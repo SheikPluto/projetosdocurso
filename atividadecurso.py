a78841f068633ba445fc5a6d7eb8023f8a213b04
SheikPluto <marcelo.alves2105@outlook.com>
2023-09-14 16:00:33 -0300

    Update atividadecurso.py

--- atividadecurso.py
+++ atividadecurso.py
@@ -1,5 +1,16 @@
+#Resposta 1
+
 andar = 20
 while andar >= 1:
     if andar != 13:
         print('Você está no andar: ', andar)
     andar -= 1
+
+
+
+    # Resposta 2
+
+    for andar in range(20, 0, -1):
+    if andar != 13:
+        print('Você está no andar: ', andar)
+        
